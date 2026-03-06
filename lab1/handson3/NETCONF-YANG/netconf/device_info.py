3d060dc5fc60adfb3dac4e07a5b16c4db0450285
Matteo Ventali <165213323+matteoventali@users.noreply.github.com>
2026-03-06 15:00:02 +0100

    Delete lab1/handson3/NETCONF-YANG/netconf/device_info.py

--- lab1/handson3/NETCONF-YANG/netconf/device_info.py
+++
@@ -1,6 +0,0 @@
-ios_xe1 = {
-	     "address": "devnetsandboxiosxec8k.cisco.com",
-             "port": 830,
-             "username": "matteo.ventali",
-             "password": "X2g3E_VsLG1alhC_"
-          }
