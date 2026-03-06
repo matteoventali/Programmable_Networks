4e57268c2fb82af22d5eb91844993c5810a6ad33
matteoventali <ventali.1985026@studenti.uniroma1.it>
2026-03-06 14:58:21 +0100

    First Commit

---
+++ lab1/handson3/NETCONF-YANG/netconf/device_info.py
@@ -0,0 +1,6 @@
+ios_xe1 = {
+	     "address": "devnetsandboxiosxec8k.cisco.com",
+             "port": 830,
+             "username": "matteo.ventali",
+             "password": "X2g3E_VsLG1alhC_"
+          }
---
+++ lab1/handson4/NETCONF-YANG/netconf/device_info.py
@@ -0,0 +1,15 @@
+devices = [
+   {
+      "address": "devnetsandboxiosxec8k.cisco.com",
+      "port": 830,
+      "username": "matteo.ventali",
+      "password": "Fmh2pb0CP_EXU3-"
+   },
+
+   {
+      "address": "devnetsandboxiosxec8k.cisco.com",
+      "port": 830,
+      "username": "matteo.ventali",
+      "password": "Fmh2pb0CP_EXU3-"
+   }
+]
