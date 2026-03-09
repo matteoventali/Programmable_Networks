880e15c61fda6e45e0f4fa13ccc0c31832b19af9
matteoventali <ventali.1985026@studenti.uniroma1.it>
2026-03-09 19:58:15 +0100

    .2

---
+++ labYangNetconf/handson5/1985026_handson5.py
@@ -0,0 +1,40 @@
+# HANDSON-5 solution
+# Student id: 1985026
+
+from device_info import devices
+from ncclient import manager
+import xmltodict
+
+# Query device info method
+def query_device(device):
+    with manager.connect(host=device['address'],
+                         port=device['port'],
+                         username=device['username'],
+                         password=device['password'],
+                         hostkey_verify=False) as m:
+
+        # Get Configuration and State Info for Interface
+        netconf_reply = m.get(netconf_filter)
+
+        # Process the XML and store in useful dictionaries
+        intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
+        intf_config = intf_details["interfaces"]["interface"]
+        
+        ifaces_list = []
+
+        for interface in intf_config:
+            ifaces_list.append(interface['name'])
+
+        return ifaces_list
+            
+
+# NETCONF filter to use
+netconf_filter = open("1985026_handson5.xml").read()
+
+if __name__ == '__main__':
+    # Scanning all the devices
+    for device in devices:
+        print(f"Querying device {device['address']}:{device['port']}")
+        iface_names = query_device(device)
+        print(f"Interfaces name: {iface_names}")
+        print("-----")
