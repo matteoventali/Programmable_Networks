278c4549f712cec8de60eacde18358d19af6e0a7
matteoventali <ventali.1985026@studenti.uniroma1.it>
2026-05-13 15:45:25 +0200

    Adding calculator

--- labDataplaneProgrammability2/calculator/h1/calc.py
+++ labDataplaneProgrammability2/calculator/h1/calc.py
@@ -74,7 +74,10 @@ def main():
         print(s)
         try:
             i,ts = p(s,0,[])
-            pkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / P4calc(op=ts[1].value,
+
+            
+
+            pkt = Ether(dst='00:04:00:00:00:00', src="00:00:00:00:00:01", type=0x1234) / P4calc(op=ts[1].value,
                                               operand_a=int(ts[0].value),
                                               operand_b=int(ts[2].value))
             pkt = pkt/' '
